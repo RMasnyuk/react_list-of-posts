05d9ccdae03c76e72d2bfc700ecabe9289c92b23
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-07-18 12:31:44 +0300

    list-of-posts

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import PostList from "./components/PostList";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      users: [],
+      comments: [],
+      foundPost: [],
+      loading: false,
+      loaded: false,
+      disabled: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.filterPosts = this.filterPosts.bind(this);
+    this.loadData = this.loadData.bind(this);
+
+  }
+
+  filterPosts(event) {
+    const inputValue = event.target.value.trim();
+    this.setState({
+      foundPost: this.state.posts.filter(post => {
+        return post.title.includes(inputValue) || post.body.includes(inputValue)
+      })
+    })
+  }
+
+  loadData(url) {
+    return fetch(url)
+      .then(res => res.json())
+  }
+
+  handleClick() {
+    this.setState({
+      loading: true,
+      disabled: true
+    });
+
+    Promise.all([
+      this.loadData('https://jsonplaceholder.typicode.com/posts'),
+      this.loadData('https://jsonplaceholder.typicode.com/users'),
+      this.loadData('https://jsonplaceholder.typicode.com/comments')
+    ])
+      .then(([posts, users, comments]) => this.setState({
+          loaded: true,
+          posts: posts,
+          users: users,
+          comments: comments
+        })
+      )
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.loaded
+          ? (
+            <div>
+              <input type="text" onInput={this.filterPosts} placeholder="search posts"/>
+              <PostList list={!this.state.foundPost.length
+                ? this.state.posts
+                : this.state.foundPost} users={this.state.users} comments={this.state.comments}/>
+            </div>
+          )
+          : (
+            <div className="container">
+              <button className="button" onClick={this.handleClick} disabled={this.state.disabled}>{this.state.loading
+                ? 'Loading...'
+                : 'Load'}
+              </button>
+            </div>
+          )
+        }
+      </>
+    );
+  }
 }
 
 export default App;
