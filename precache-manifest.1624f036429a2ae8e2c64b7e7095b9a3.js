d0303b3c411f4e8781fb6685a2ef4b059250e924
Rostislav Masniuk <rostislav.masnyuk.98@gmail.com>
2019-07-18 12:44:41 +0300

    Updates

---
+++ precache-manifest.1624f036429a2ae8e2c64b7e7095b9a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aefbb550f0af265be99b8789e7ae094",
+    "url": "/react_list-of-posts/index.html"
+  },
+  {
+    "revision": "f30de2e999985b20cd6e",
+    "url": "/react_list-of-posts/static/css/main.55f9d8d7.chunk.css"
+  },
+  {
+    "revision": "ade41769240f0323820d",
+    "url": "/react_list-of-posts/static/js/2.18512419.chunk.js"
+  },
+  {
+    "revision": "f30de2e999985b20cd6e",
+    "url": "/react_list-of-posts/static/js/main.92f41ec5.chunk.js"
+  },
+  {
+    "revision": "a64777f12689cbd25d48",
+    "url": "/react_list-of-posts/static/js/runtime~main.98cc979e.js"
+  },
+  {
+    "revision": "e5fb2d880c11bbd056a0b8d9c5c96fd6",
+    "url": "/react_list-of-posts/static/media/bg3.e5fb2d88.jpg"
+  },
+  {
+    "revision": "471a1ad342659289433e05a611d206f8",
+    "url": "/react_list-of-posts/static/media/user.471a1ad3.png"
+  }
+]);
